c59b531c6daa925a2d54d48d68f16c48b0aa513d
saikiran <saikiran.sadurla@zemosolabs.com>
2022-08-11 09:51:53 +0530

    new commit read

--- assignment-2/src/components/atoms/TypoWithIcon/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TypoWithIcon = () => {
-  return (
-    <div>TypoWithIcon</div>
-  )
-}
-
-export default TypoWithIcon
--- assignment-2/src/components/molecules/BookDetailTabs/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BookDetailsTabs = () => {
-  return (
-    <div>BookDetailsTabs</div>
-  )
-}
-
-export default BookDetailsTabs
